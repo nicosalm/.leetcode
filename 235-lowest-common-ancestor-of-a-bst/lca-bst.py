9b80812995f924147b96d59261e5d0c72333e533
Nico <nico@nicosalm.io>
2024-04-06 23:19:56 -0500

    235

---
+++ 235-lowest-common-ancestor-of-a-bst/lca-bst.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        root_val = root.val
+        p_val = p.val
+        q_val = q.val
+
+        if p_val > root_val and q_val > root_val:
+            return self.lowestCommonAncestor(root.right, p, q)
+        
+        elif p_val < root_val and q_val < root_val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        
+        # we have found the split point (LCA)
+        else:
+            return root
+
+'''
+Notes:
+Recursion in O(n) with O(n) Space
+'''
